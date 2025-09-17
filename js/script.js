9284fa87ac5c0fa199c3c1d134b0fa766b6c29f7
Ilhom <nzzmmvv@gmail.com>
2025-09-17 13:41:33 +0500

    add a db

--- js/script.js
+++ js/script.js
@@ -246,13 +246,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify({ name: "Alex" }),
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  fetch("http://localhost:3000/menu")
+    .then((data) => data.json())
+    .then((res) => console.log(res));
 });
