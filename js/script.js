cd1e6cd8d3d351c3efa67320be703fc990b4f6df
Ilhom <nzzmmvv@gmail.com>
2025-09-22 15:12:30 +0500

    add the slider

--- js/script.js
+++ js/script.js
@@ -249,4 +249,50 @@ window.addEventListener("DOMContentLoaded", () => {
       setTimeout(closeModal, 4000);
     }
   }
+
+  // Slider
+
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    current = document.querySelector("#current"),
+    total = document.querySelector("#total");
+
+  let slideIndex = 1;
+  showSlide(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlide(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlide(n) {
+    showSlide((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlide(-1);
+  });
+  next.addEventListener("click", () => {
+    plusSlide(+1);
+  });
 });
